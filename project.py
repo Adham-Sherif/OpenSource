1c3dd6bd0eb1e995e04ab4c9d1702e5c58165899
omarreda512 <149432994+omarreda512@users.noreply.github.com>
2023-12-23 21:07:56 +0200

    apply6button and radio buttons (add noise)

--- project.py
+++ project.py
@@ -579,6 +579,40 @@ class MyGUI(QMainWindow):
             self.label_13.setAlignment(Qt.AlignCenter)        
         else:
             QMessageBox.warning(self, "Error", "Please open an image first.")
+#-------------------------group6------------------------------------
+        self.pushButton_17.clicked.connect(self.apply6_button_clicked)
+    def apply6_button_clicked(self):
+        if hasattr(self, 'original_image'):
+            selected_radio = self.get_selected_radio_in_groupbox_10()
+            print ("selected_radio in apply_button_clicked is ",selected_radio)
+            if selected_radio:
+                # Perform actions based on the selected radio button
+                if selected_radio == "Salt A Pepper":
+        
+                    self.apply_salt_and_pepper_noise()
+                    
+                elif selected_radio == "Uniform":
+                 
+                    self.add_uniform_noise()   
+                    
+                
+                   
+  
+            else:
+                # If no radio button is selected, show a message box
+                QMessageBox.warning(self, "No Radio Button Selected", "Please select a radio button!")
+        else:
+            QMessageBox.warning(self, "Error", "Please open an image first.")
+    def get_selected_radio_in_groupbox_10(self):
+        group_box = self.findChild(QGroupBox, "groupBox_10")
+        if group_box:
+            for child in group_box.findChildren(QRadioButton):
+                if child.isChecked():
+                    print("Selected radio button:", child.text())  # Check which radio button is selected
+                    return child.text()
+        else:
+            print("groupBox_10 not found")
+        return None
 def main():
     app = QApplication(sys.argv)
     window = MyGUI()
