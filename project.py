03e45e6469113a04d85b6e79f51789aa77ec64e5
Omar87Magdy <omarr90081@gmail.com>
2023-12-23 20:45:55 +0200

    Add convertion to gray button

--- project.py
+++ project.py
@@ -29,6 +29,7 @@ class MyGUI(QMainWindow):
         self.show()
         self.exitButton.clicked.connect(self.exit_application)
         self.pushButton_18.clicked.connect(self.open_image)
+        self.garybutton.clicked.connect(self.convert_to_grayscale)
 
         #group 2 button 
         self.pushButton_13.clicked.connect(self.apply2_button_clicked)
@@ -475,6 +476,18 @@ class MyGUI(QMainWindow):
                 self.image_path = fname
             else:
                 QMessageBox.warning(self, "Error", "Could not open or display the image.")
+    def convert_to_grayscale(self):
+        if hasattr(self, 'original_image'):
+            grayscale_image = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2GRAY)
+            height, width = grayscale_image.shape
+            q_img = QImage(grayscale_image.data, width, height, width, QImage.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(q_img)
+            pixmap = pixmap.scaled(self.label_13.width(), self.label_13.height(), aspectRatioMode=Qt.KeepAspectRatio)
+            self.label_13.setPixmap(pixmap)
+            self.label_13.setAlignment(Qt.AlignCenter)
+        else:
+            QMessageBox.warning(self, "Error", "Please open an image first.")
+
 def main():
     app = QApplication(sys.argv)
     window = MyGUI()
